40248c0b47218bfc33ff0329fc048e865c2db3da
sersmaa12 <82792595+sersmaa12@users.noreply.github.com>
2025-12-04 15:57:37 +0800

    Delete Dataset directory

--- Dataset/download_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-echo && echo Downloading Authors100 dataset && echo
-curl https://transfer.sh/z8FLg/Authors.zip -o Authors.zip
-
-echo && echo Extracting Authors100 dataset && echo
-unzip Authors.zip
